c1c7d62f6d17c2c50d4ff6bc2c3cbbc8a4e77f33
khatrn <khatran081996@gmail.com>
2020-12-17 10:29:56 +0700

    Modify: Thay đổi header về vị trí đúng

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
         <h2>CSS Layout Float</h2>
         <p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
         <p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>
+        <header>
+            <h2>Cities</h2>
+        </header>
         <section>
             <nav>
                 <ul>
                 <li><a href="#">London</a></li>
                 <li><a href="#">Paris</a></li>
                 <li><a href="#">Tokyo</a></li>
-                </ul>asa
+                </ul>
             </nav>
             
             <article>
@@ -21,9 +24,6 @@
                 <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
             </article>
         </section>
-        <header>
-            <h2>Cities</h2>
-        </header>
         <footer>
             <p>Footer</p>
         </footer>
